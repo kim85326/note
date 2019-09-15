3f3ae422bf37eef5fb6a8c7f9f1b7e8eef871c01
elaine <kim85326@gmail.com>
2019-09-15 22:51:25 +0800

    Update 2019-09-15T14:51:21.661Z

---
+++ js/common.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var app = document.getElementById("app");
+var theme = document.getElementById("theme");
+var lightThemeButton = theme.querySelector(".view-mode[data-mode=light]");
+var darkThemeButton = theme.querySelector(".view-mode[data-mode=dark]");
+theme.addEventListener("click", function (event) {
+  if (!event.target.classList.contains("view-mode")) {
+    return;
+  }
+
+  if (event.target.dataset.mode === "dark") {
+    lightThemeButton.classList.remove("active");
+    darkThemeButton.classList.add("active");
+    app.classList.add("dark");
+  } else {
+    lightThemeButton.classList.add("active");
+    darkThemeButton.classList.remove("active");
+    app.classList.remove("dark");
+  }
+});
+//# sourceMappingURL=common.js.map
